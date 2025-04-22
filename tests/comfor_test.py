7df89e7e1b6f7dcececb8aed7c93c49b46458136
Kunologist <2014709936@qq.com>
2025-04-22 18:17:37 +0800

    fix: adapt to refactored classifiers

--- tests/comfor_test.py
+++ tests/comfor_test.py
@@ -11,16 +11,14 @@ from pathlib import Path
 def test_on_demo_images():
     from mllm_defake.classifiers.basic_classifier import ComForClassifier
 
-    device = "cuda:0"
+    device = "cuda"
     real_samples = Path("demo/real").rglob("*")
     fake_samples = Path("demo/fake").rglob("*")
     community_forensics_checkpoint_path = Path("local/comfor/model_v11_ViT_224_base_ckpt.pt")
     input_size = "224"
     classifier = ComForClassifier(
         community_forensics_checkpoint_path,
-        real_samples,
-        fake_samples,
         input_size=input_size,
         device=device,
     )
-    classifier.evaluate("local/comfor.csv")
+    classifier.evaluate(real_samples, fake_samples, "local/comfor.csv")
